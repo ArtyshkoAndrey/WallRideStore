b9c57559169e083e402f9d5098b82119b1d34a56
Fulliton <artyshko.andrey@gmail.com>
2020-04-05 16:04:06 +0700

    Update footer.blade.php
    Данные магазина в футере

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,13 +2,13 @@
   <div class="container">
     <div class="row">
       <div class="col-12 col-md text-center text-white">
-        <p>+7 (777) 777 - 77 - 77</p>
+        <a href="tel:+77475562383">+7(747) 556-23-83</a>
       </div>
       <div class="col-12 col-md text-center text-white">
-        <p>+7 (777) 777 - 77 - 77</p>
+         <a href="mailto:info@wallridestore.kz">info@wallridestore.kz</a>   
       </div>
       <div class="col-12 col-md text-center text-white">
-        <a href="mailto:info@wallridestore.kz">info@wallridestore.kz</a>
+        <a href="https://www.instagram.com/wallride_store/">inst:@wallride_store</a>
       </div>
     </div>
   </div>
