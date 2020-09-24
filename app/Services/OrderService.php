d39a368a18f1a6a3b73fda80d4179c4a7ec77d78
Fulliton <artyshko.andrey@gmail.com>
2020-09-24 14:28:15 +0700

    Фикс автоудаление заказов

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -18,88 +18,89 @@ use App\Exceptions\CouponCodeUnavailableException;
 
 class OrderService
 {
-    public function store(User $user, $address, $items, $payment_method, $express_company, $cost_transfer, CouponCode $coupon = null)
-    {
-        // 如果传入了优惠券，则先检查是否可用
-        if ($coupon) {
-            $coupon->checkAvailable($user);
-        }
-        // 开启一个数据库事务
-        $order = \DB::transaction(function () use ($user, $address, $items, $coupon, $payment_method, $express_company, $cost_transfer) {
-
-            $order   = new Order([
-              'address'      => [
-                  'address'       => ''.Country::find($address['country'])->name.','.City::find($address['city'])->name.','. $address['street'],
-                  'contact_name'  => $address['contact_name'],
-                  'contact_phone' => $address['phone'],
-              ],
-              'total_amount' => 0,
-              'id_express_company' => $express_company,
-              'payment_method' => $payment_method,
-              'ship_price' => $cost_transfer
-            ]);
-            $order->user()->associate($user);
-            $order->save();
-
-            $totalAmount = 0;
-            $ids = array();
-            foreach ($items as $data) {
-              for($i = 0; $i < $data['amount']; $i++) {
-                array_push($ids, $data['productSku']['id']);
-              }
-            }
-            $productsSku = Product::getProducts($ids);
-            $priceAmount = 0;
-            foreach ($productsSku as $k => $productSku) {
-              $priceAmount += $productSku->product->on_sale ? (int)$productSku->product->price_sale : (int)$productSku->product->price;
-            }
-            foreach ($items as $data) {
-              $sku = ProductSku::find($data['productSku']['id']);
-              $item = $order->items()->make([
-                'amount' => $data['amount'],
-                'price' => $sku->product->on_sale ? $sku->product->price_sale : $sku->product->price,
-              ]);
-              $item->product()->associate($sku->product_id);
-              if (isset($sku->skus->title)) {
-                $item->product_sku = $sku->skus->title;
-              } else {
-                $item->product_sku = 'One Size';
-              }
-              $item->save();
-              if ($sku->decreaseStock($data['amount']) <= 0) {
-                throw new InvalidRequestException('Товар распродан');
-              }
-            }
-            $totalAmount = $priceAmount;
-            if ($coupon) {
-                $coupon->checkAvailable($user, $priceAmount);
-                $totalAmount = $coupon->getAdjustedPrice($priceAmount, $items);
-//                dd($totalAmount);
-                $order->couponCode()->associate($coupon);
-                if ($coupon->changeUsed() <= 0) {
-                    throw new CouponCodeUnavailableException('该优惠券已被兑完');
-                }
-            }
-//            dd($totalAmount);
-            $order->update(['total_amount' => $totalAmount]);
-
-            $skuIds = collect($items)->pluck('sku_id')->all();
-            app(CartService::class)->remove($skuIds);
-
-            return $order;
-        });
-
-        // dispatch(new CloseOrder($order, config('app.order_ttl')));
-
-        return $order;
+  public function store(User $user, $address, $items, $payment_method, $express_company, $cost_transfer, CouponCode $coupon = null)
+  {
+    // 如果传入了优惠券，则先检查是否可用
+    if ($coupon) {
+      $coupon->checkAvailable($user);
     }
+    // 开启一个数据库事务
+    $order = \DB::transaction(function () use ($user, $address, $items, $coupon, $payment_method, $express_company, $cost_transfer) {
 
-    public function cancled(Order $order) {
-//      Order::SHIP_STATUS_CANCEL
-      $order->ship_status = Order::SHIP_STATUS_CANCEL;
+      $order   = new Order([
+        'address'      => [
+          'address'       => ''.Country::find($address['country'])->name.','.City::find($address['city'])->name.','. $address['street'],
+          'contact_name'  => $address['contact_name'],
+          'contact_phone' => $address['phone'],
+        ],
+        'total_amount' => 0,
+        'id_express_company' => $express_company,
+        'payment_method' => $payment_method,
+        'ship_price' => $cost_transfer
+      ]);
+      $order->user()->associate($user);
       $order->save();
-      $order->user->notify(new OrderCancledNotification($order));
-      foreach ($order->items as $item) {
+
+      $totalAmount = 0;
+      $ids = array();
+      foreach ($items as $data) {
+        for($i = 0; $i < $data['amount']; $i++) {
+          array_push($ids, $data['productSku']['id']);
+        }
+      }
+      $productsSku = Product::getProducts($ids);
+      $priceAmount = 0;
+      foreach ($productsSku as $k => $productSku) {
+        $priceAmount += $productSku->product->on_sale ? (int)$productSku->product->price_sale : (int)$productSku->product->price;
+      }
+      foreach ($items as $data) {
+        $sku = ProductSku::find($data['productSku']['id']);
+        $item = $order->items()->make([
+          'amount' => $data['amount'],
+          'price' => $sku->product->on_sale ? $sku->product->price_sale : $sku->product->price,
+        ]);
+        $item->product()->associate($sku->product_id);
+        if (isset($sku->skus->title)) {
+          $item->product_sku = $sku->skus->title;
+        } else {
+          $item->product_sku = 'One Size';
+        }
+        $item->save();
+        if ($sku->decreaseStock($data['amount']) <= 0) {
+          throw new InvalidRequestException('Товар распродан');
+        }
+      }
+      $totalAmount = $priceAmount;
+      if ($coupon) {
+        $coupon->checkAvailable($user, $priceAmount);
+        $totalAmount = $coupon->getAdjustedPrice($priceAmount, $items);
+//                dd($totalAmount);
+        $order->couponCode()->associate($coupon);
+        if ($coupon->changeUsed() <= 0) {
+          throw new CouponCodeUnavailableException('该优惠券已被兑完');
+        }
+      }
+//            dd($totalAmount);
+      $order->update(['total_amount' => $totalAmount]);
+
+      $skuIds = collect($items)->pluck('sku_id')->all();
+      app(CartService::class)->remove($skuIds);
+
+      return $order;
+    });
+
+    // dispatch(new CloseOrder($order, config('app.order_ttl')));
+
+    return $order;
+  }
+
+  public function cancled(Order $order) {
+//      Order::SHIP_STATUS_CANCEL
+    $order->ship_status = Order::SHIP_STATUS_CANCEL;
+    $order->save();
+    $order->user->notify(new OrderCancledNotification($order));
+    foreach ($order->items as $item) {
+      if (Product::find($item->product->id)) {
         $sku = ProductSku::where('product_id', $item->product->id);
         if($sku->count() === 1) {
           $sku = $sku->first();
@@ -108,10 +109,20 @@ class OrderService
           $sku = $sku->whereHas('skus', function ($q) use ($item) {
             $q->where('skuses.title', $item->product_sku);
           })->first();
-          $sku->addStock($item->amount);
+          if ($sku) {
+            $sku->addStock($item->amount);
+          } else {
+            $sku        = new ProductSku();
+            $sku->stock = $item->amount;
+            $sku->product()->associate($item->product->id);
+            $sku->skus()->associate(Skus::where('title', $item->product_sku)->first());
+            $sku->save();
+          }
+
         } else {
           throw new \Exception('Ошибка в размерах');
         }
       }
     }
+  }
 }
