00f5e87af72dab75d2a8fee24a51def9c612a48c
Fulliton <artyshko.andrey@gmail.com>
2021-02-16 21:21:43 +0700

    Кеширования брендов для главного экрана

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Contracts\Foundation\Application;
@@ -10,6 +11,7 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Cache;
 
 
 class HomeController extends Controller
@@ -30,7 +32,11 @@ class HomeController extends Controller
 //      ->take(4)
 //      ->get();
 //    return view('user.index', compact('categories', 'newProducts', 'hitProducts'));
-    return view('user.index');
+
+    $brands = Cache::remember('brands-to-index', config('app.cache.bd'), function () {
+      return Brand::where('to_index', true)->take(6)->get();
+    });
+    return view('user.index', compact('brands'));
   }
 
   /**
