1883af42f93a3116583774a63622cbac0b5fd0ff
Fulliton <artyshko.andrey@gmail.com>
2021-02-19 19:55:47 +0700

    Вывод информации товара в шаблоне

--- resources/views/user/layouts/item.blade.php
+++ resources/views/user/layouts/item.blade.php
@@ -5,29 +5,37 @@
       <div class="col-12 p-0">
         <a href="#" class="img-wrapper d-block">
           <picture>
-            <source type="image/webp" srcset="{{ asset('images/product.jpg') }}">
-            <source type="image/jpeg" srcset="{{ asset('images/product.jpg') }}">
-            <img src="{{ asset('images/product.jpg') }}" class="w-100 h-100" alt="...">
+            <source type="image/webp" srcset="{{ $product->thumbnail_webp }}">
+            <source type="image/jpeg" srcset="{{ $product->thumbnail_jpg }}">
+            <img src="{{ $product->thumbnail_jpg }}" class="w-100 h-100" alt="{{ $product->title }}">
           </picture>
           <div class="info">
-            <p class="py-2 mb-0 text-white px-3">L / M / XXL</p>
+            <p class="py-2 mb-0 text-white px-3">
+              @foreach($product->skuses as $skus)
+                @if($loop->last)
+                  {{ $skus->title }}
+                @else
+                  {{ $skus->title }} /
+                @endif
+              @endforeach
+            </p>
           </div>
         </a>
       </div>
     </div>
     <div class="row pt-3 mb-auto">
       <div class="col-12 p-0">
-        <a class="title" href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, quidem.</a>
+        <a class="title" href="{{ route('product.show', $product->id) }}">{{ $product->title }}</a>
       </div>
     </div>
     <div class="row context mt-auto">
       <div class="col-md-5 col-12 d-flex flex-column align-self-end p-0">
-{{--        @if($item->on_sale)--}}
-          <span class="old-price">123 000 @{{ $store.state.currency.symbol }}</span>
-          <span class="price">120 000 @{{ $store.state.currency.symbol }}</span>
-{{--        @else--}}
-{{--          <span class="price">123 000 @{{ $store.state.currency.symbol }}</span>--}}
-{{--        @endif--}}
+        @if($product->on_sale)
+          <span class="old-price">{{ $cost($store.state.currency.ratio * <? echo $product->price ?>) }} @{{ $store.state.currency.symbol }}</span>
+          <span class="price">{{ $cost($store.state.currency.ratio * <? echo $product->price_sale ?>) }} @{{ $store.state.currency.symbol }}</span>
+        @else
+          <span class="price">{{ $cost($store.state.currency.ratio * <? echo $product->price ?>) }} @{{ $store.state.currency.symbol }}</span>
+        @endif
       </div>
       <div class="col-md-7 mt-3 col-12 p-0">
 {{--        @if($item->skuses()->count() >=  2)--}}
