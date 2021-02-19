f501472ab16fb60b1369b771aab0d616ea37efc7
Fulliton <artyshko.andrey@gmail.com>
2021-02-19 17:18:45 +0700

    Перевод локализации на Ru в  административной панели

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -21,6 +22,7 @@ class Admin
 //      Если залогинен
       if (auth()->user()->is_admin) {
 //        Если в свойствах стоит Admin
+        App::setLocale('ru');
         return $next($request);
       }
     }
