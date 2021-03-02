8020a90dcd6b38c6dfe7993220ce351615f62d6b
Fulliton <artyshko.andrey@gmail.com>
2021-03-02 17:08:31 +0700

    Локализация ошибок

---
+++ resources/lang/en/errors_redirect.php
@@ -0,0 +1,10 @@
+<?php
+return [
+  "product" => [
+    "product_show" => "This product does not exist",
+    "product_catalog_skuses" => "Wrong dimensions selected",
+    "product_catalog_brands" => "Wrong brands selected",
+    "product_catalog_categories" => "Wrong categories selected",
+    "product_search" => "Search field cannot be empty",
+  ]
+];
---
+++ resources/lang/ru/errors_redirect.php
@@ -0,0 +1,10 @@
+<?php
+return [
+  "product" => [
+    "product_show" => "Данного товара не существует",
+    "product_catalog_skuses" => "Выбраны неверные размеры",
+    "product_catalog_brands" => "Выбраны неверные бренды",
+    "product_catalog_categories" => "Выбраны неверные категории",
+    "product_search" => "Поле поиск не может быть пустым"
+  ]
+];
