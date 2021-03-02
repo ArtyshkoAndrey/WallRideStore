7ddb697a94ff74a1403d1a0eb9bb3f32075ed841
Fulliton <artyshko.andrey@gmail.com>
2021-03-02 17:20:53 +0700

    Рефакторинг проверки на администратора

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use App;
+use App\Exceptions\RedirectWithErrorsException;
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -15,18 +17,21 @@ class Admin
    * @param Request $request
    * @param Closure $next
    * @return mixed
+   * @throws App\Exceptions\RedirectWithErrorsException
    */
   public function handle(Request $request, Closure $next)
   {
     if (Auth::check()) {
-//      Если залогинен
-      if (auth()->user()->is_admin) {
-//        Если в свойствах стоит Admin
+//    If logged in
+      $user = User::find(auth()->id());
+      if ($user->is_admin) {
+//      If the properties are Admin
+//      Localization fix
         App::setLocale('ru');
         return $next($request);
       }
     }
 
-    return redirect()->route('index')->withErrors(['Доступно только администраторам!']);
+    throw new RedirectWithErrorsException(__('errors_redirect.auth.admin'));
   }
 }
