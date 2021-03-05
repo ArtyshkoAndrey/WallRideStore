bfe4501d31d1bfb4e96c20f99e8e76c295b50484
Fulliton <artyshko.andrey@gmail.com>
2021-03-05 21:26:40 +0700

    Базовые страницы для новостей

---
+++ resources/views/user/post/index.blade.php
@@ -0,0 +1,17 @@
+@extends('user.layouts.app')
+@section('title', 'Новости')
+
+@section('content')
+  <div class="container mt-2 cart-page pb-5">
+    <div class="row justify-content-center">
+      <div class="col-12">
+        <h2 class="font-weight-bolder">Новости</h2>
+      </div>
+    </div>
+  </div>
+@endsection
+
+
+@section('js')
+
+@endsection
