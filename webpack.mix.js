9878ebb95f79ddd8a58349e996f781354df2ad27
Fulliton <artyshko.andrey@gmail.com>
2021-05-02 12:45:12 +0700

    mix 6.0

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,13 +3,13 @@ let productionSourceMaps = false;
 
 mix.options({processCssUrls: false})
 
-mix.js('resources/js/user/app.js', 'public/js')
-  .sass('resources/sass/user/app.scss', 'public/css')
+mix.js('resources/js/user/app.js', 'public/js').vue({ version: 2 })
+  .sass('resources/sass/user/app.scss', 'public/css');
 
 
-mix.js('resources/js/admin/app.js', 'public/js/admin')
+mix.js('resources/js/admin/app.js', 'public/js/admin').vue({ version: 2 })
   .sass('resources/sass/admin/app.scss', 'public/css/admin')
-  .sourceMaps(productionSourceMaps, 'source-map')
+  .sourceMaps(productionSourceMaps, 'source-map');
 
 if (mix.inProduction()) {
   mix.version();
