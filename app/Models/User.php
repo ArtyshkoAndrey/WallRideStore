da527b6400942e4639d52ccd0be2622e8533127d
Fulliton <artyshko.andrey@gmail.com>
2022-04-01 12:22:24 +0700

    User Model PHP DOC

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,43 +7,50 @@
 namespace App\Models;
 
 use Eloquent;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\DatabaseNotification;
-use Illuminate\Notifications\DatabaseNotificationCollection;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
+use Database\Factories\UserFactory;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 
 /**
  * App\Models\User
  *
- * @property int $id
- * @property string $name
- * @property string|null $avatar
- * @property string $email
- * @property Carbon|null $email_verified_at
- * @property string $password
- * @property string|null $address
- * @property string|null $post_code
- * @property string|null $phone
- * @property bool $notification
- * @property bool $old_notification
- * @property int|null $currency_id
- * @property bool $is_admin
- * @property string|null $remember_token
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @property-read \App\Models\City $city
- * @property-read \App\Models\Country $country
- * @property-read \App\Models\Currency|null $currency
- * @property-read string $user_image
+ * @property int                                                        $id
+ * @property string                                                     $name
+ * @property string|null                                                $avatar
+ * @property string                                                     $email
+ * @property Carbon|null                                                $email_verified_at
+ * @property string                                                     $password
+ * @property string|null                                                $address
+ * @property string|null                                                $post_code
+ * @property string|null                                                $phone
+ * @property bool                                                       $notification
+ * @property bool                                                       $old_notification
+ * @property int|null                                                   $currency_id
+ * @property bool                                                       $is_admin
+ * @property string|null                                                $remember_token
+ * @property Carbon|null                                                $created_at
+ * @property Carbon|null                                                $updated_at
+ * @property int|null                                                   $country_id
+ * @property int|null                                                   $city_id
+ * @property-read City                                                  $city
+ * @property-read Country                                               $country
+ * @property-read Currency|null                                         $currency
+ * @property-read string                                                $user_image
  * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
- * @property-read int|null $notifications_count
+ * @property-read int|null                                              $notifications_count
+ * @property-read Collection|CartItem[]                                 $cartItems
+ * @property-read int|null                                              $cart_items_count
+ * @property-read string                                                $full_address
+ * @property-read Collection|Order[]                                    $orders
+ * @property-read int|null                                              $orders_count
  * @method static Builder|User newModelQuery()
  * @method static Builder|User newQuery()
  * @method static Builder|User query()
@@ -55,6 +62,9 @@ use Illuminate\Support\Carbon;
  * @method static Builder|User whereEmailVerifiedAt($value)
  * @method static Builder|User whereId($value)
  * @method static Builder|User whereIsAdmin($value)
+ * @method static Builder|User whereCityId($value)
+ * @method static Builder|User whereCountryId($value)
+ * @method static UserFactory factory(...$parameters)
  * @method static Builder|User whereName($value)
  * @method static Builder|User whereNotification($value)
  * @method static Builder|User whereOldNotification($value)
@@ -64,21 +74,12 @@ use Illuminate\Support\Carbon;
  * @method static Builder|User whereRememberToken($value)
  * @method static Builder|User whereUpdatedAt($value)
  * @mixin Eloquent
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\CartItem[] $cartItems
- * @property-read int|null $cart_items_count
- * @property-read string $full_address
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Order[] $orders
- * @property-read int|null $orders_count
- * @property int|null $country_id
- * @property int|null $city_id
- * @method static Builder|User whereCityId($value)
- * @method static Builder|User whereCountryId($value)
- * @method static \Database\Factories\UserFactory factory(...$parameters)
  */
 class User extends Authenticatable
 {
   use HasFactory, Notifiable;
 
+  public const PHOTO_PATH = 'storage/users/';
   /**
    * The attributes that are mass assignable.
    *
@@ -94,9 +95,8 @@ class User extends Authenticatable
     'is_admin',
     'phone',
     'notification',
-    'old_notification'
+    'old_notification',
   ];
-
   /**
    * The attributes that should be hidden for arrays.
    *
@@ -106,7 +106,6 @@ class User extends Authenticatable
     'password',
     'remember_token',
   ];
-
   /**
    * The attributes that should be cast to native types.
    *
@@ -119,14 +118,12 @@ class User extends Authenticatable
     'old_notification' => 'boolean',
   ];
 
-  const PHOTO_PATH = 'storage/users/';
-
   /**
    * User city
    *
    * @return BelongsTo
    */
-  public function city (): BelongsTo
+  public function city(): BelongsTo
   {
     return $this->belongsTo(City::class);
   }
@@ -136,43 +133,43 @@ class User extends Authenticatable
    *
    * @return BelongsTo
    */
-  public function country (): BelongsTo
+  public function country(): BelongsTo
   {
     return $this->belongsTo(Country::class);
   }
 
-  public function currency (): belongsTo
+  public function currency(): belongsTo
   {
     return $this->belongsTo(Currency::class);
   }
 
-  public function getUserImageAttribute (): string
+  public function getUserImageAttribute(): string
   {
     return $this->avatar ? asset(self::PHOTO_PATH . $this->avatar) : asset('images/product.jpg');
   }
 
-  public function cartItems (): HasMany
+  public function cartItems(): HasMany
   {
     return $this->hasMany(CartItem::class, 'user_id', 'id');
   }
 
-  public function orders (): HasMany
+  public function orders(): HasMany
   {
     return $this->hasMany(Order::class);
   }
 
-  public function getFullAddressAttribute (): string
+  public function getFullAddressAttribute(): string
   {
     $text = '';
-    if($this->country) {
+    if ($this->country) {
       $text .= $this->country->name . ', ';
     }
 
-    if($this->city) {
+    if ($this->city) {
       $text .= $this->city->name . ', ';
     }
 
-    if($this->address) {
+    if ($this->address) {
       if ($this->post_code) {
         $text .= $this->address . ', ';
       } else {
@@ -180,7 +177,7 @@ class User extends Authenticatable
       }
     }
 
-    if($this->post_code) {
+    if ($this->post_code) {
       $text .= $this->post_code;
     }
 
